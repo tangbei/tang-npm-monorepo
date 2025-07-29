e3b7628b27ca487ba97a66ee4675f51dc5b1e67a
唐贝 <itangbei@sina.com>
2025-07-29 12:00:54 +0800

    增加beta版配置

--- packages/npm-helper/scripts/version.js
+++ packages/npm-helper/scripts/version.js
@@ -3,6 +3,7 @@ const util = require('util');
 const fs = require('fs');
 const path = require('path');
 const inquirer = require('inquirer');
+const execAsync = util.promisify(exec);
 
 // 获取package.json中的包名
 let packageJson, packageName;
